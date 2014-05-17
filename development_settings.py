ec79a0029f98e8803230966c307a7608e4ac303d
Joe21 <joej21@gmail.com>
2014-05-17 03:50:07 -0400

    about to sync sql3 db to virtual dev environment

--- development_settings.py
+++ development_settings.py
@@ -2,3 +2,15 @@
 from .settings import *
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '/Users/user/More-Projects/athletes_root_folder/athletesproject/sqlite3database.db',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
+    }
+}
