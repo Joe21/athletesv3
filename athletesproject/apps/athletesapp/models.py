068a74270a294bc0f6993b4247c76cc4022945d0
Joe21 <joej21@gmail.com>
2014-05-17 04:06:56 -0400

    both db's linked and models added. proceeding to push to both remotes

--- athletesproject/apps/athletesapp/models.py
+++ athletesproject/apps/athletesapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Athlete(models.Model):
+	RECOMMENDATIONS = (
+		('a', 'Hire Joe IMMEDIATELY!'),
+		('b', 'Hire Joe NOW!')
+		)
+	first_name = models.CharField(max_length=50)
+	last_name = models.CharField(max_length=50)
+	sport = models.CharField(max_length=50)
+	recommendation = models.CharField(max_length=1, choices=RECOMMENDATIONS)
+
+	def __unicode__(self):
+		return self.first_name
